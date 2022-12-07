babe7648c841afe925c03257d263d3d19f529bed
Tristan Bandat <tristan.bandat@gmail.com>
2022-12-07 13:33:01 +0100

    Optical changes in main file

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ from ImageDataset import ImageDataset
 from utils import collate_fn
 
 
-
-#TODO: prototype, improve
+# TODO: prototype, improve
 def evaluate_model(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader):
     model.eval()
     loss_fn = torch.nn.L1Loss()
@@ -22,7 +21,6 @@ def evaluate_model(model: torch.nn.Module, dataloader: torch.utils.data.DataLoad
     with torch.no_grad():
         for batch in dataloader:
             image_array, target_array = batch
-
             # change dimensions of image_array to fit CNN
             image_array = image_array.permute(0, 3, 1, 2)
             # move to device
@@ -31,13 +29,12 @@ def evaluate_model(model: torch.nn.Module, dataloader: torch.utils.data.DataLoad
             # get output
             output = model(image_array)
             loss += loss_fn(output, target_array)
-
     model.train()
     loss /= len(dataloader)
     return loss
 
 
-def main(results_path="results", device: torch.device = torch.device("cuda:0"), n_updates: int = int(50000)):
+def main(results_path="results", device: torch.device = torch.device("cuda:0"), n_updates: int = int(5)):
     # TODO: Maybe change this to 3 separate files, but this is easier for now
     image_dataset = ImageDataset(frame_path=join(os.getcwd(), 'data.pkl'))
     train_set = torch.utils.data.Subset(image_dataset, indices=np.arange(int(len(image_dataset) * (3 / 5))))
@@ -54,8 +51,8 @@ def main(results_path="results", device: torch.device = torch.device("cuda:0"), 
 
     writer = SummaryWriter(log_dir=os.path.join(results_path, 'tensorboard'))
 
-    print_stats_at = 100  # print status to tensorboard every x updates
-    validate_at = 5000  # evaluate model on validation set and check for new best model every x updates
+    print_stats_at = 1  # print status to tensorboard every x updates
+    validate_at = 5  # evaluate model on validation set and check for new best model every x updates
     update = 0  # current update counter
     best_validation_loss = np.inf  # best validation loss so far
     no_update_counter = 0  # counter for how many updates have passed without a new best validation loss
@@ -73,37 +70,28 @@ def main(results_path="results", device: torch.device = torch.device("cuda:0"), 
     # Save initial model as "best" model (will be overwritten later)
     torch.save(net, os.path.join(results_path, 'best_model.pt'))
 
-    update_progess_bar = tqdm.tqdm(total=n_updates, desc=f"loss: {np.nan:7.5f}", position=0)  # progressbar
+    update_progress_bar = tqdm.tqdm(total=n_updates, desc=f"loss: {np.nan:7.5f}", position=0)  # progressbar
     while update < n_updates and no_update_counter < 3:
         for batch in train_loader:
             # get data
             image_array, target_array = batch
-
-
             # change dimensions of image_array to fit CNN
             image_array = image_array.permute(0, 3, 1, 2)
-
             image_array = image_array.to(device)
             target_array = target_array.to(device)
             # Reset gradients
             optimizer.zero_grad()
-
             # Forward pass
             output = net(image_array)
-
             # Calculate loss
             loss = loss_fn(output, target_array)
-
             # Backward pass
             loss.backward()
-
             # Update weights
             optimizer.step()
-
             # Update progress bar
-            update_progess_bar.set_description(f"loss: {loss.item():7.5f}")
-            update_progess_bar.update(1)
-
+            update_progress_bar.set_description(f"loss: {loss.item():7.5f}")
+            update_progress_bar.update(1)
             # Evaluate model on validation set
             if (update + 1) % validate_at == 0 and update > 0:
                 val_loss = evaluate_model(net, dataloader=val_loader)
@@ -132,7 +120,7 @@ def main(results_path="results", device: torch.device = torch.device("cuda:0"), 
             if update >= n_updates or no_update_counter >= 3:
                 break
 
-    update_progess_bar.close()
+    update_progress_bar.close()
     print('Finished Training!')
 
     # Load best model and compute score on test set
@@ -146,8 +134,6 @@ def main(results_path="results", device: torch.device = torch.device("cuda:0"), 
     print(f"test loss: {test_loss}")
     print(f"validation loss: {val_loss}")
     print(f"training loss: {train_loss}")
-    # Demo Code
-
 
 
 if __name__ == '__main__':
