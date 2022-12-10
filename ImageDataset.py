c4b8ba4ffd0def6d6019f198d23b094a8adacb33
Tristan Bandat <tristan.bandat@gmail.com>
2022-12-10 17:24:48 +0100

    Improve performance of dataset class

--- ImageDataset.py
+++ ImageDataset.py
@@ -4,21 +4,19 @@ from torch.utils.data import Dataset
 
 class ImageDataset(Dataset):
     def __init__(self, frame_path):
-        self.pkl = None
         self.image_files = list()
         # load frames from pickle
         with open(frame_path, "rb") as f:
-            self.pkl = pickle.load(f)
+            pkl = pickle.load(f)
 
-        height = self.pkl[0]['image'].shape[0]
-        width = self.pkl[0]['image'].shape[1]
+        self.height = pkl[0]['image'].shape[0]
+        self.width = pkl[0]['image'].shape[1]
 
         # iterate over frames
-        for f in self.pkl:
+        for f in pkl:
             # convert to numpy array
-            np_image = f['image'].numpy().reshape(1, height, width)
-            boxes = f['boxes'].numpy().reshape(1, height, width)
-
+            np_image = f['image'][None, :]
+            boxes = f['boxes'][None, :]
             self.image_files.append({'image': np_image, 'boxes': boxes})
 
     def __getitem__(self, index):
